6fe5972e4546e02244dd5f263f955bb1c60343e0
wwf9w0 <aliemrekaradag1@gmail.com>
2025-10-26 09:36:12 +0300

    refactor

--- src/main/java/org/virtual/annotation/Main.java
+++ src/main/java/org/virtual/annotation/Main.java
@@ -1,15 +1,11 @@
 package org.virtual.annotation;
 
-import org.virtual.core.VirtualLahmacunChef;
-import org.virtual.core.VirtualThreadOven;
-import org.virtual.virtual.model.TaskLahmacunResult;
 
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         VirtualFlowRuntime runtime = new VirtualFlowRuntime();
-        runtime.startAllFlows("org.virtual.annotation"); // burası sen
+        runtime.startAllFlows("org.virtual.annotation");
 
     }
 }
