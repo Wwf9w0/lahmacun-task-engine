ab96af118376e0c6c4e9e38c6b845c8fbf74edbd
wwf9w0 <aliemrekaradag1@gmail.com>
2025-10-26 19:41:12 +0300

    added try to virtual thread

--- src/main/java/org/virtual/core/VirtualLahmacunTask.java
+++ src/main/java/org/virtual/core/VirtualLahmacunTask.java
@@ -4,17 +4,10 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 public class VirtualLahmacunTask<T> {
-    private int offset;
-    private int timeunitMillis;
-    private String key;
     private final CompletableFuture<T> future;
 
     public VirtualLahmacunTask(Supplier<T> supplier) {
         this.future = CompletableFuture.supplyAsync(supplier);
-    }
-
-    public T join() {
-        return future.join();
     }
 
     public CompletableFuture<T> future() {
