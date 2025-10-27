cf73af215029476d0baefd48038c529786ddb720
wwf9w0 <aliemrekaradag1@gmail.com>
2025-10-27 11:22:20 +0300

    added scanner for test

--- src/main/java/org/virtual/Main.java
+++ src/main/java/org/virtual/Main.java
@@ -3,11 +3,16 @@ package org.virtual;
 import org.virtual.core.VirtualLahmacunFlowRuntime;
 import org.virtual.queue.QueueManager;
 
+import java.util.Scanner;
+
 public class Main<T> {
 
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the package name");
+        String packageName = in.next();
         var queueManager = new QueueManager<>();
         var runtime = new VirtualLahmacunFlowRuntime<>(queueManager);
-        runtime.startAllFlows("org.virtual.annotation");
+        runtime.startAllFlows(packageName);
     }
 }
